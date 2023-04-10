7de4120ddaace6404f89a7648d9f91cee5ecb4b0
Luthfi A Pratama <luthfianump@gmail.com>
2023-04-10 13:56:17 +0700

    initial

---
+++ src/routes/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import PingController from "../controller/ping.controller";
+
+const router = express.Router();
+
+router.get("/ping", async (_req, res) => {
+  const controller = new PingController();
+  const response = await controller.getMessage();
+  return res.send(response);
+});
+
+export default router;
